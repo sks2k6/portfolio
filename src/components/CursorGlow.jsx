ce7622f98f841c273c485b24c04076fdf41fa541
sks2k6 <108617100+sks2k6@users.noreply.github.com>
2025-05-13 01:23:57 +0530

    Update CursorGlow.jsx

--- src/components/CursorGlow.jsx
+++ src/components/CursorGlow.jsx
@@ -5,13 +5,13 @@ import { motion } from "framer-motion";
 const CursorGlow = ({ mousePosition }) => {
   return (
     <motion.div
-      className="pointer-events-none fixed inset-0 z-30 transition duration-300 cursor-glow"
+      className="pointer-events-none fixed inset-0 z-30 transition duration-200 cursor-glow"
       style={{
         "--x": `${mousePosition.x}px`,
         "--y": `${mousePosition.y}px`,
       }}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
+      initial={{ opacity: 0.5 }}
+      animate={{ opacity: 2 }}
       transition={{ duration: 0.5 }}
     />
   );
